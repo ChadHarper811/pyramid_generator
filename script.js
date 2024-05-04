0b25d502ca2f571456263472007896f7f527f696
Chad Harper <chadharper811@gmail.com>
2024-05-04 15:02:14 -0500

    added for loop

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const character = "Hello";
+const character = "#";
 const count = 8;
 const rows = [];
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+};
+let result = "";
+console.log(result);
