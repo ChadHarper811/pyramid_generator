a97cf81d1709e8b29d5dd72709cdd521a93ae4f0
Chad Harper <chadharper811@gmail.com>
2024-05-04 15:19:35 -0500

    added function

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const character = "#";
 const count = 8;
 const rows = [];
+
+function padRow(name) {
+    return character + name;
+};
+
+const call = padRow("Chad");
+console.log(call);
+
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(i);
+    rows.push(character.repeat(i + 1));
 };
 let result = "";
+
+for (const row of rows) {
+    result = result + "\n" + row;
+};
 console.log(result);
