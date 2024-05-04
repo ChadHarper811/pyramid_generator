7f0d55691260360dce5cff0ded251628ee7d525a
Chad Harper <chadharper811@gmail.com>
2024-05-04 14:49:13 -0500

    initialized variables

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
