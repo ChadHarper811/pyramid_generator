1fea28738cb05f4bbbcb4db783cc5a7741f02703
Chad Harper <chadharper811@gmail.com>
2024-05-04 16:15:09 -0500

    set pyramid to regular

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const character = "#";
 const count = 8;
 const rows = [];
-let inverted = true;
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber -1) + " ".repeat(rowCount - rowNumber);
