79832c68048d2b1b826c6b257b43d4dce34441c4
Chad Harper <chadharper811@gmail.com>
2024-05-04 14:52:15 -0500

    reseting variables

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let character = 'Hello';
+let character = "Hello";
 let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
-let popped = rows.pop();
-console.log(popped);
-console.log(rows);
+let rows = [];
